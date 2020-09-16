713ff790e701a44ee13f827207e013ed2af97c1d
Arniel <aceba1@outlook.com>
2020-09-15 23:53:48 -0400

    Update middleware & add JWT_SECRET

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 //const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-//const body = require('body-parser'); //TODO: remove body-parser
+//TODO: remove body-parser
 const userRouter = require('./userRouter');
+
+//ENV CONTANTS
+const 
+    PORT = process.env.PORT || 4000, //Port number for server to listen on, defined in enviorment file
+    URI = process.env.MONGO; // URI that gives read/write access to database, this need to be stored in an env so that the public can not read/write your database without going through your API     
 
 // express app
 const app = express();
@@ -23,5 +30,7 @@ app.use((req, res, next) => {
 app.use('/user', userRouter);
 
 
+//Connect to Data-Base
+//mongoConnect(URI)
 // listen for requests
 app.listen(4000);
--- server/userRouter.js
+++ server/userRouter.js
@@ -1,21 +1,40 @@
 const express = require("express");
+const validateUser = require('./middleware/validateUser');
+const checkUserCreds = require('./middleware/checkUserCreds');
+const createJWT = require('./middleware/createJWT');
 
 const router = express.Router();
 
-// router.use('/', (req, res, next) => {
-//     res.send('get /user ?');
-//     console.log('get /user ?');
-//     next();
-// });
+router.post('/register', 
+    validateUser, 
+    async (req, res) => {
+        console.log(req.body);
+        try {
+            //req.userData is defined by the validation middleware
+            //const newUser = await User.create(req.userData);
+            res.status(201).send('Received /register');//.json(newUser);
 
-router.use('/login', (req, res) => {
-    console.log(req.body);
-    res.status(200).send('Received login!');
+        } catch (err) {
+            res.status(500).json({
+                message: err.message,
+                error: err
+            })
+        }
 });
 
-router.use('/register', (req, res) => {
-    console.log(req.body);
-    res.status(200).send('Received register!');
+router.put('/login', 
+    checkUserCreds,
+    createJWT,
+    async (req, res) => {
+        console.log(req.body);
+        try {
+            res.json(req.token);
+        } catch (err) {
+            res.status(500).json({
+                message: err.message,
+                error: err
+            })
+        }
 });
 
 module.exports = router;

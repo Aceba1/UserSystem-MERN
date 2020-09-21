dc3a203cfb0f9e196304909046aa91f09ffbeceb
Arniel <aceba1@outlook.com>
2020-09-20 23:15:46 -0400

    Change home page for when logged in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import Button from './Button'
+import User from '../utils/user';
 
 /*window.location.origin*/ 
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome back</p>
+export default function Home(props) {
+  console.log(props);
+  if (props.loggedIn) {
+    return (
       <div>
-        <Button text='Login' onClick={() => { window.location = '/login' }} />
-        <br/>
-        <Button text='Register' onClick={() => { window.location = '/register' }} />
+        <h1>Home</h1>
+        <p>Welcome back, {props.user}!</p>
+        <div>
+          <Button text='Logout' onClick={() => { User.logOut(); window.location = '/' }} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Welcome! Please login or register</p>
+        <div>
+          <Button text='Login' onClick={() => { window.location = '/login' }} />
+          <br/>
+          <Button text='Register' onClick={() => { window.location = '/register' }} />
+        </div>
+      </div>
+    )
+  }
 }

98615c6301f3482cbc3238d4bba865ba9369fb0a
Arniel <aceba1@outlook.com>
2020-09-20 23:13:50 -0400

    Update App.js,verify login and use localStorage theming hooks

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,62 @@ import Button from "./components/Button.js";
 import Styles from "./utils/styles";
 import AppRouter from "./components/AppRouter";
 //import Cookies from "./utils/cookies";
+import Warnings from "./components/Warnings.js";
+import User from './utils/user';
 
 export default class App extends React.Component { //TODO: Create AppRouter
 
   constructor() {
     super();
     this.state = {
-      style: Styles.currentStyle
+      style: Styles.currentStyle,
+      siteErrors: [],
+      showSiteError: false
     };
+    this.setThemeLight = this.setThemeLight.bind(this);
+    this.setThemeDark = this.setThemeDark.bind(this);
+    this.loginSuccess = this.loginSuccess.bind(this);
+    this.failedToLogin = this.failedToLogin.bind(this);
+  }
+  
+  componentDidMount() {
+    User.verifyLoggedIn(this.loginSuccess, this.failedToLogin); // Can add TrueEvent and FalseEvent here
+  }
+
+  loginSuccess() {
+    console.log('Logged in as ' + User.user + '!');
+  }
+
+  failedToLogin(error = '') {
+    this.setState({
+      siteErrors: [error, 'Please log back in'],
+      showSiteError: true
+    })
+  }
+
+  setThemeLight() { this.setTheme(Styles.light) }
+  setThemeDark() { this.setTheme(Styles.dark) }
+
+  setTheme(theme) {
+    localStorage.setItem('theme', theme);
+    Styles.currentStyle = theme; 
+    this.setState({style: theme});
   }
 
   render() {
     return (
-      <div className={"App " + this.state.style} /*style={this.state.style.app}*/ >
+      <div id='App' className={"App " + this.state.style} /*style={this.state.style.app}*/ >
+        <Warnings 
+          items={this.state.siteErrors}
+          showWarning={this.state.showSiteError}
+        />
         <Button 
           text='Set Dark'
-          onClick={() => {
-            Styles.currentStyle = Styles.dark; 
-            this.setState({style: Styles.dark});
-            Cookies.setValue('theme', 'dark');
-            //style={Styles.light.button}
-          }}
+          onClick={this.setThemeDark}
         />
         <Button 
           text='Set Light'
-          onClick={() => {
-            Styles.currentStyle = Styles.light; 
-            this.setState({style: Styles.light});
-            Cookies.setValue('theme', 'light');
-          }}
-          //style={Styles.light.button}
+          onClick={this.setThemeLight}
         />
         <hr/>
         <AppRouter/>

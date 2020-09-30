84df7baf0813030ead4b8f42694973054c13afed
Arniel <aceba1@outlook.com>
2020-09-30 10:31:44 -0400

    Use module.exports

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,11 +1,19 @@
-const storageAccess = {
+module.exports = {
+  /**
+   * @param {string} key ID of value
+   * @param {any} initial Initial value, if not defined
+   * @returns {initial} Same type as initial
+   */
   get: (key, initial) => {
     const data = localStorage.getItem(key);
     if (data) return JSON.parse(data);
     return initial;
   },
+  /**
+   * @param {string} key ID of value
+   * @param {any} value The new value
+   */
   set: (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   }
 } 
-export default storageAccess;
